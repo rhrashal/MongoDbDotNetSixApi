e86eae416de46ee49b8af0a918fedba2f93440ca
rhrashal <rhrashal@gmail.com>
2022-01-30 15:16:06 +0600

    initial upload

---
+++ mongoDbDotNetSixApi/Services/IStudentService.cs
@@ -0,0 +1,13 @@
+﻿using mongoDbDotNetSixApi.Models;
+
+namespace mongoDbDotNetSixApi.Services
+{
+    public interface IStudentService
+    {
+        List<Student> Get();
+        Student Get(string id);
+        Student Create(Student student);
+        void Update(string id, Student student);
+        void Remove(string id);
+    }
+}
